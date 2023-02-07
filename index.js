1e0f9e691a1eae92864b4b6f243ef240ab03e97e
Aan Sulaiman <mac@Macs-MacBook-Pro.local>
2023-02-07 14:28:57 +0700

    http methods

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 
 const app = express()
 
+// pattrn -> app.method('path', handler())
+
+app.use("/", (req, res, next) => {
+    res.json({
+        nama: "Aan Sulaiman",
+        email: "aansulaiman92@gmail.com"
+    })
+})
+
+
 app.listen(4000, () => {
     console.log(`Listening on port http://localhost:4000`)
 })
