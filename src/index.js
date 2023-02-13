488bb6f4794f2f507d0ba671a5ec59366687df39
aansulaimann <aansulaiman92@gmail.com>
2023-02-13 22:05:25 +0700

    file upload with multer

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const usersRoutes = require('./routes/users.js')
 
 // middleware
 const middlewareLogReq = require('./middleware/logs.js')
+const upload = require('./middleware/multer.js')
 
 const app = express()
 
@@ -18,6 +19,18 @@ app.use('/assets', express.static('public/image'))
 
 // pattrn -> app.method('path', handler())
 app.use('/users', usersRoutes)
+
+app.post('/upload', upload.single('photo'), (req, res) => {
+    res.json({
+        message: "Upload berhasil"
+    })
+})
+
+app.use((err, req, res, next) => {
+    res.json({
+        message: err.message
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port http://localhost:${PORT}`)
